db31163cb9e8dff72668467375c38b30afb0e248
Derrick Mar <derrickmar1215@berkeley.edu>
2014-08-18 02:55:12 -0700

    puts current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,4 +24,17 @@ class SessionsController < Devise::SessionsController
 	# 	# 	return
 	# 	# end
 	# end
+
+	  # POST /resource/sign_in
+	  def create
+	  	p 'in create!'
+	  	self.resource = warden.authenticate!(auth_options)
+	  	set_flash_message(:notice, :signed_in) if is_flashing_format?
+	  	sign_in(resource_name, resource)
+	  	yield resource if block_given?
+	  	respond_to do |format|
+	  		format.html { respond_with resource, location: after_sign_in_path_for(resource) }
+	  		format.json { render :json => { user: self.resource, success: true } }
+	  	end
+	  end
 end
