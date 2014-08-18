96a286a88f5de9eee9e74100894d6d3d20d130d7
Derrick Mar <derrickmar1215@berkeley.edu>
2014-08-18 01:08:54 -0700

    test login session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,24 @@ class SessionsController < Devise::SessionsController
 
 	def create
 		puts "In sessions controller"
+		puts auth_options
+		puts params
+		# puts resource.valid_password?
 		self.resource = warden.authenticate!(auth_options)
 		set_flash_message(:notice, :signed_in) if is_flashing_format?
+		puts "before sign_in"
 		sign_in(resource_name, resource)
-		yield resource if block_given?
+		puts "before signed_in? called"
+		puts signed_in?(:user)
 		respond_to do |format|
 			format.html { redirect_to after_sign_in_path_for(resource) }
 			format.json { render :json => { user: resource, success: true } }
 		end
+
+		# if resource.valid_password?(params[:user_login][:password])
+		# 	sign_in("user", resource)
+		# 	render :json=> {:success=>true, :auth_token=>resource.authentication_token, :login=>resource.login, :email=>resource.email}
+		# 	return
+		# end
 	end
 end
