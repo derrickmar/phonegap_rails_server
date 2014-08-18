e2ec14d75092d9293825faf77816cd04fe605894
Derrick Mar <derrickmar1215@berkeley.edu>
2014-08-18 04:07:37 -0700

    puts current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -39,4 +39,22 @@ class SessionsController < Devise::SessionsController
 			format.json { render :json => { user: self.resource, success: true } }
 		end
 	end
+
+	def destroy
+		p 'In DESTROY'
+		signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+		set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
+		yield if block_given?
+		respond_to_on_destroy
+	end
+
+	def respond_to_on_destroy
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+    	format.json { render :json => { success: true } }
+    	format.all { head :no_content }
+    	format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+    end
+end
 end
